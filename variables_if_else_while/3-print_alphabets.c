67f78ee5115501eacc35d17541316061d1b41101
Matthew Ernst <6628@holbertonstudents.com>
2023-05-25 09:02:31 -0700

    allalpha

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Return: Return 0
+ * letter - letter to be incrimented
+ */
+int main(void)
+{
+	int letter;
+
+	for (letter = 'a'; letter <= 'Z'; letter++)
+	putchar(letter);
+	{
+		putchar('\n');
+	}
+		return (0);
+}
