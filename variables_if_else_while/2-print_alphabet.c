7ae9c295cb3dd61a3a110383790833c2a60eab92
Matthew Ernst <6628@holbertonstudents.com>
2023-05-25 08:29:36 -0700

    alphaloop

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Return: Return
+ * letter - letter to be incrimented
+ */
+int main(void)
+{
+        int letter;
+
+        for (letter = 'a'; letter <= 'z'; letter++)
+                putchar(letter);
+	{
+		putchar('\n');
+	}
+		return (0);
+}
