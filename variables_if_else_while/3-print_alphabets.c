ae2424b4b9629acce71e3135def59c2ffa9eb26c
Matthew Ernst <6628@holbertonstudents.com>
2023-05-25 09:40:33 -0700

    helpstocapitalizeyourlatters

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -10,7 +10,7 @@ int main(void)
 
 	for (letter = 'a'; letter <= 'z'; letter++)
 		putchar(letter);
-	for (letter = 'a'; letter <= 'z'; letter++)
+	for (letter = 'A'; letter <= 'Z'; letter++)
 		putchar(letter);
 
 	{
