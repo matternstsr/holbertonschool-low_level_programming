1d3a36b236d25fc26fc526e0d9ab41ee1517323c
Matthew Ernst <6628@holbertonstudents.com>
2023-05-25 10:47:38 -0700

    nunmersof10:wq

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Return: Return 0
+ * letter - letter to be incrimented
+ */
+int main(void)
+{
+	int decimal;
+
+	for (decimal = 0; decimal <= 9; decimal++)
+	{
+		printf("%d", decimal);
+	}
+	putchar('\n');
+	return (0);
+}
