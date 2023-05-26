d341b4bade8855840083e5784a48d41f2c6a835b
Matthew Ernst <6628@holbertonstudents.com>
2023-05-26 08:23:54 -0700

    changedbacktoaftertestingi

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -6,14 +6,14 @@
  */
 int main(void)
 {
-	char a[] = "_putchar";
-	int s = sizeof(a);
-	int b;
-	
-	for (b = 0; b < s; b++)
-	{
-		_putchar(a[b]);
-	}
-	_putchar('\n');
-	return (0);
+        char a[] = "_putchar";
+        int s = sizeof(a);
+        int b;
+
+        for (b = 0; b < s; b++)
+        {
+                _putchar(a[b]);
+        }
+        _putchar('\n');
+        return (0);
 }
