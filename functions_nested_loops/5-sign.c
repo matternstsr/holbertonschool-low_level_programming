5ad28dc5a2b0262cc3f0d1471e1a469aebea06f2
Matthew Ernst <6628@holbertonstudents.com>
2023-05-26 14:48:44 -0700

    asciiforresults

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isalpha - Checks if a character is alphabetic.
+ * @n: Variable Input.
+ *
+ * Return: Returns 1 and prints + if n is greater than zero
+ * Return: Returns 0 and prints 0 if n is zero
+ * Return: Returns -1 and prints - if n is less than zero
+ */
+
+int print_sign(int n)
+{
+
+	if (n = 1)
+
+		return (48);
+	
+	else if (n < 1)
+
+                return (152);
+
+	else
+
+		return (43);
+
+}
