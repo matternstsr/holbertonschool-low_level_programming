36c117aa9d80e6e9ced5cdc2b4a5f1d8516f9059
Matthew Ernst <6628@holbertonstudents.com>
2023-05-26 13:28:23 -0700

    githubcrashedcatchup

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+*_islower - function to check for lowercase character
+*Return: 1 if c is lower, else 0
+*/
+
+int _islower(int c)
+{
+
+	if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
+
+		return (1);
+
+	else
+
+		return (0);
+
+}
