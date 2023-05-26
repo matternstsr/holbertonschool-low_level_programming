7473c12f9df548b75daa6572661df79b26538dd3
Matthew Ernst <6628@holbertonstudents.com>
2023-05-26 13:50:01 -0700

    commentformattingissues

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include "main.h"
 /**
-*c:cholds the value of a-z and A-Z
-*isalpha:function to check for lower and upper character
-*Return: 1 if c is lower or upper, else 0
-*/
+ *c:cholds the value of a-z and A-Z
+ *isalpha:function to check for lower and upper character
+ *Return: 1 if c is lower or upper, else 0
+ */
 
 int _isalpha(int c)
 {
