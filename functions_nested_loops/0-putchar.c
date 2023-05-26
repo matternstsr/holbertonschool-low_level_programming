024b6d585c0a3d66dbc5718cf27cf951c686eca1
Matthew Ernst <6628@holbertonstudents.com>
2023-05-26 08:34:50 -0700

    copiedspaceswhenremadefile

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -6,14 +6,15 @@
  */
 int main(void)
 {
-        char a[] = "_putchar";
-        int s = sizeof(a);
-        int b;
+	char a[] = "_putchar";
+	int s = sizeof(a);
+	int b;
 
-        for (b = 0; b < s; b++)
-        {
-                _putchar(a[b]);
-        }
-        _putchar('\n');
-        return (0);
+	for (b = 0; b < s; b++)
+	{
+		_putchar(a[b]);
+	}
+
+	_putchar('\n');
+	return (0);
 }
