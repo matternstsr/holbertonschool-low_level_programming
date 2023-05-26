e98bb8bc3f2bbff5351a6f7af0ff96a87625651f
Matthew Ernst <6628@holbertonstudents.com>
2023-05-26 12:53:55 -0700

    islower1

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * int _islower(int c) - prints 0 if lowercase
+ * Return: void
+ */
+int _islower(int c)
+
+{
+	int lowercase = "c";
+
+	if islower(lowercase)
+		
+	return (0)
+}
