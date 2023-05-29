e726b9b81cffa5f1b4663d4aa4462dd5e1fa89a8
Matthew Ernst <6628@holbertonstudents.com>
2023-05-29 08:32:01 -0700

    absolutevulaueusing_abs

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  * _abs - Calculates the absolute value of an integer.
+  * @c: The number to be computed.
+  *
+  * Return: Absolute value of number or zero
+  */
+
+int _abs(int)
+{
+int x = -4, y;
+ 
+    y = _abs(x);
+ 
+    printf("The absolute value of x is %d.\n", y);
+}
