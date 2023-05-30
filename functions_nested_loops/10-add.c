0b733ca3dbcf759eb8978573b86f03978a34ec75
Matthew Ernst <6628@holbertonstudents.com>
2023-05-29 20:32:36 -0700

    adding2togther

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+  * add - adds two integers and returns the result.
+  *
+  * Return: empty output
+  */
+int add(int a, int b)
+{
+    int sum = a + b;
+    return sum;
+}
