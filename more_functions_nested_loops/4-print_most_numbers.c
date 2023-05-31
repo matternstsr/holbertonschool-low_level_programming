3240dbb70b7116d8f2ffdc313a7f9204aca91cc2
Matthew Ernst <6628@holbertonstudents.com>
2023-05-31 09:13:36 -0700

    tryingout4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_numbers - checks if the value 0 and prints 1-9 followed by new line
+ * Return: not return
+ */
+
+void print_numbers(void)
+{
+	char c;
+
+	for (c = 48 ; c <= 57 ; c++)
+		if ((C != 50) || ((c! = 52)
+		{
+		_putchar(c);
+		}
+		else
+		{
+		c++
+		}	
+	_putchar('\n');
+}
