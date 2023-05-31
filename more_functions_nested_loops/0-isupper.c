119d448c7cf49b4d533c421ff33d82c26d1d2d37
Matthew Ernst <6628@holbertonstudents.com>
2023-05-31 07:08:37 -0700

    0isupper

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isupper - prints 1 if uppper and 0 if lower
+ * @c: used to hold the testing value uppper or lower letter
+ * Return: prints 1 if uppper and 0 if loower
+ */
+
+void int _isupper(int c)
+{
+if (c > a && c < z)
+	{
+	return (0);
+	}
+else
+	{
+	return (1);
+	}
+}
