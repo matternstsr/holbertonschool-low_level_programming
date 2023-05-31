64ecb24e57189b29e8068028ab16198d73da054d
Matthew Ernst <6628@holbertonstudents.com>
2023-05-31 08:20:35 -0700

    atimeb

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - checks if the value of c is a digit
+ * @a: value 1
+ * @b: value 2
+ * Return: returns value
+ */
+
+int mul(int a, int b)
+{
+( a * b )
+	return (0);
+}
