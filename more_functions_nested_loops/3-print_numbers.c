bc4ec2d252b3b2cb5e84489ff065530f614f451b
Matthew Ernst <6628@holbertonstudents.com>
2023-05-31 08:49:22 -0700

    trying3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_numbers - checks if the value 0 and prints 1-9 followed by new line
+ * @c: used to hold the value of 0-9
+ * Return: not return
+ */
+
+void print_numbers(void)
+{
+	int c;
+	(c = 48)
+	while ((c >= 48) && (c <= 57))
+		{
+		putchar(c)
+		}
+	else
+		{
+		putchar('\n')
+		}
+}
