e1f9f079ccf215514696e32cdbd6e551c9a1e39e
Matthew Ernst <6628@holbertonstudents.com>
2023-05-31 10:52:23 -0700

    tryingagain

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -3,6 +3,7 @@
 /**
  * more_numbers - prints 1-14 x 10 followed by new line
  * Return: not return
+ *
  */
 
 void more_numbers(void)
@@ -10,11 +11,11 @@ void more_numbers(void)
 	char c;
 	int r;
 	c = '0';
-	r= '1';
+	r = '1';
 
 	for (r = 1 ; r <= 10; r++)
 	{
-		for (c = 0; c <= 14;c++)
+		for (c = 0; c <= 14; c++)
 		{
 		_putchar(c);
 		}
