442b9f9600beaabda65d93ccdee559c5aae41359
Matthew Ernst <6628@holbertonstudents.com>
2023-05-31 10:17:52 -0700

    trying5withnewlayotut

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 1-14 x 10 followed by new line
+ * Return: not return
+ */
+
+void more_numbers(void);
+{
+	char c;
+	int r;
+	c = '0'
+	c = '0'
+
+	for (r >= '0' ; r <= '10';)
+		if (c >= '0' ; c <= '14';)
+		{
+		_putchar(c);
+		c++;
+		}
+		if (c = '14' ; r++;)
+	_putchar('\n');
+}
