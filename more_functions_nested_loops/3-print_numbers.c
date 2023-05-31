ba2f36a24be8c999e0630b0c3178f2f9f7c760e6
Matthew Ernst <6628@holbertonstudents.com>
2023-05-31 09:04:04 -0700

    switcheditupthinkihavetouseascii

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -2,22 +2,16 @@
 
 /**
  * print_numbers - checks if the value 0 and prints 1-9 followed by new line
- * @c: used to hold the value of 0-9
  * Return: not return
  */
 
 void print_numbers(void)
 {
-	int c;
-	(c = 48);
-	if (c <= 57)
-		{
-		_putchar(c);
-		c++;
-		}
+	char c;
 
-	else
-		{
-		_putchar('\n');
-		}
+	for (c = 48 ; c <= 57 ; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
 }
