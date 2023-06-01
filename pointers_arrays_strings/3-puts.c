daeaa0dd8ff72567e6c0c9ee0c7757ba8a784dae
Matthew Ernst <6628@holbertonstudents.com>
2023-06-01 16:03:22 -0700

    stoutfromstrtonull

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _puts - used to write a string to the standard output (stdout) to null
+ * @str: value stored as a string
+ * Return: void
+ *
+ */
+
+void _puts(char *str)
+{
+	puts(str);
+	return 0;
+}
