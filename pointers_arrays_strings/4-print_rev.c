7e883a98dd086623a35d7e2dc1ce19a921ee27af
Matthew Ernst <6628@holbertonstudents.com>
2023-06-01 16:32:01 -0700

    trythis.notsurewhymyotherwouldntwork.

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -9,8 +9,14 @@
 
 void print_rev(char *s)
 {
-	while (*s)
-		_putchar(*s--);
-
+	int i = 0;
+	while (s[i] != '\0')
+		{
+		i++;
+		}
+	for (i--; i >= 0; i--)
+		{
+		_putchar(s[i]);
+		}
 	_putchar('\n');
 }
