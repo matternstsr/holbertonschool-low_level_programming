92553ae94934fb1522e520b395a4008bf43ee578
Matthew Ernst <6628@holbertonstudents.com>
2023-06-01 12:59:40 -0700

    cantbethatsimple...

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -4,10 +4,10 @@
  * reset_to_98 - takes a pntr to an int value that pnts to to 98
  * @n: the value that is passed to the pointer
  * Return: void
+ *
  */
 
 void reset_to_98(int *n)
 {
-*n = 98;
-foo(&n);
+    *n = 98;
 }
