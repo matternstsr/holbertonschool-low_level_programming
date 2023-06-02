b02511794c08d3b1751a5be19afd6e1e74c30f94
Matthew Ernst <6628@holbertonstudents.com>
2023-06-02 09:47:56 -0700

    planb

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,28 +1,25 @@
 #include "main.h"
 
 /**
- * _strcpy - used to print the array at the set value set by n
- * @dest: destination of the buffer
- * @src: source of the string to be copied
- * Return: void
+ * _atoi - Changes a string to an integer.
+ * @s: the string that will be changed.
  *
+ * Return: the string in a integer form.
  */
-
 int _atoi(char *s)
 {
+	int msigns;
+	(msigns= 1);
 	int numbers = 0;
-	int signs = 1;
-	const char *p;
-	(*p = *s);
-	if (*p == '-')
-	{
-		signs = -1;
-		p++;
+
+	do {
+		if (*s == '-')
+			msigns *= -1;
+		else if (*s >= '0' && *s <= '9')
+			numbers = (numbers * 10) + (*s - '0');
+		else if (numbers > 0)
+			break;
 	}
-	while (*p != '\0')
-	{
-		numbers = numbers * 10 + (*p - '0');
-		p++;
-	}
-	return (signs * numbers);
+	while (*s++);
+	return (numbers * msigns);
 }
