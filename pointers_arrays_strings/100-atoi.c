7b0ee4d474ffc8c7b454cb777e17f5f89e47e550
Matthew Ernst <6628@holbertonstudents.com>
2023-06-02 09:28:48 -0700

    ifoundhalfofthisscriptsounderstandhalflol

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strcpy - used to print the array at the set value set by n
+ * @dest: destination of the buffer
+ * @src: source of the string to be copied
+ * Return: void
+ *
+ */
+
+int _atoi(char *s)
+{
+	int numbers = 0;
+	int signs = 1;
+	const char *p;
+	(*p = s);
+	if (*p == '-')
+	{
+		signs = -1;
+		p++;
+	}
+	while (*p != '\0')
+	{
+		numbers = numbers * 10 + (*p - '0');
+		p++;
+	}
+	return (signs * numbers);
+}
