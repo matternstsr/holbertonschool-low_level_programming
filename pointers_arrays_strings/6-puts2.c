ad6fa8f7b57b20cbffd75f27c057cab057956c76
Matthew Ernst <6628@holbertonstudents.com>
2023-06-01 17:39:05 -0700

    everyothercharinthestring

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * puts2 - used to pull every 2 char in a string value
+ * @*str: pointer for a string str
+ * Return: void
+ *
+ */
+
+void puts2(char *str)
+{
+        for (; *str; str += 2)
+                {
+                putchar(*str);
+                }
+        putchar('\n');
+}
