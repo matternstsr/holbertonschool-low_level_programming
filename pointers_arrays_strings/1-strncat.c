a4b4109688d86b700f89024ecbcca15014c36045
Matthew Ernst <6628@holbertonstudents.com>
2023-06-07 06:57:48 -0700

    stop_at_n_incrimentation.

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strcat - adds src string to the end dest string
+ * @dest: Destination of the concatinated string
+ * @src: Source
+ *
+ * Return: pointer to dest string
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int ptr = 0;
+	int ptr2 = 0;
+
+	while (dest[ptr] != '\0')
+	{
+		ptr++;
+	}
+
+	while (src[ptr2] != '\0')
+	{
+		if  (ptr2 < n)
+		dest[ptr] = src[ptr2];
+		ptr++;
+		ptr2++;
+	}
+	dest[ptr] = '\0';
+
+	return (dest);
+}
