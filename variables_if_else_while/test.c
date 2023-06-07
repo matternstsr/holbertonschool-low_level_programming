1a38402ea1707a51de881866eecf0eaadcaed8bd
Matthew Ernst <6628@holbertonstudents.com>
2023-06-06 21:31:26 -0700

    all

---
+++ variables_if_else_while/test.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Return: Return 0
+ */
+int main(void)
+{
+	int numbers;
+
+	for (numbers = 0; numbers <= 9; numbers++)
+	{
+		putchar(numbers + '0');
+		if (numbers == 9)
+
+		putchar(',');
+		putchar(' ');
+	}
+
+	putchar('\n');
+
+	return (0);
+}
