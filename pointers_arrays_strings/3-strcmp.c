093b735ad0ab9a24a35d9d1b27abb39c93c022e8
Matthew Ernst <6628@holbertonstudents.com>
2023-06-07 09:16:30 -0700

    possible_spaced_indent

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strncpy - adds src string to the end dest string
+ * @dest: Destination of the concatinated string
+ * @src: Source
+ * @n: value that stops the incrimentation of the destination pointer.
+ * Return: pointer to dest string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+	(i = 0);
+	while (src[i] != '\0')
+	{
+		if (i < n)
+		{
+		dest[i] = src[i];
+		i++;
+		}
+		else
+			break;
+
+	}
+	for ( ; i < n; i++)
+                   dest[i] = '\0';
+	return (dest);
+}
