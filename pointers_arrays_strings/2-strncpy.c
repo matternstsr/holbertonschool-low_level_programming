448333d464b4a57270f2fef1bdda6c75ee3eb209
Matthew Ernst <6628@holbertonstudents.com>
2023-06-07 08:40:53 -0700

    added_a_break_if_meets_n

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -10,11 +10,17 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	while (src[n] != '\0')
+	int i;
+	(i = 0);
+	while (src[i] != '\0')
 	{
-		dest[n] = src[n];
-		n++;
+		if (i < n)
+		dest[i] = src[i];
+		i++;
+		else
+			break;
+
 	}
-	dest[n] = '\0';
+	dest[i] = '\0';
 	return (dest);
 }
