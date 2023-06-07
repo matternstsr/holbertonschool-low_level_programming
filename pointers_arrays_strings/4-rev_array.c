4b8cbb2fac47657a54d4fa47b20da269e6dc11af
Matthew Ernst <6628@holbertonstudents.com>
2023-06-07 15:37:55 -0700

    reversed

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+  * reverse_array - reverses the array
+  * @a: pointer to the value of a
+  * @n: number of ints in the array
+  *
+  * Return: 0
+  */
+
+void reverse_array(int *a, int n)
+{
+	int (*p = a);
+	int (i = 1);
+	int x;
+	int (j = 0);
+
+	while  (i < n)
+	{
+		i++;
+		p++;
+	}
+	while ( j < i / 2)
+	{
+		x = a[j];
+		a[j] = *p;
+		*p = x;
+		p--;
+	}
+}
