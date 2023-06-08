caec52a58da8ae953be288513ac20735898c4282
Matthew Ernst <6628@holbertonstudents.com>
2023-06-08 12:29:54 -0700

    not_sure_about_the_tabs

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -12,10 +12,14 @@ char *_strchr(char *s, char c)
 {
 	int a;
 
-	for (a = 0; s[a] != '\0' || s[a] != '\0' + 1; a++)
+	for (a = 0; s[a] != '\0'; a++)
 	{
 		if (s[a] == c)
-			return (&s[a]);
-	}
-		return (NULL);
+			return (&s[a];
+					}
+					if (s[a] == c)
+					{
+					return (&s[a]);
+					}
+					return (NULL);
 }
