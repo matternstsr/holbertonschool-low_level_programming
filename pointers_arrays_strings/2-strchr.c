157a955ea90a8988257d3fac21d28e432beee207
Matthew Ernst <6628@holbertonstudents.com>
2023-06-08 12:19:52 -0700

    if_it_=_c_to_the_null_or_the_null_rewrite

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -3,6 +3,7 @@
 /**
  * _strchr - searches string till it find a char def by c or null if not.
  * @s: sting of info
+ * @s: info that is selekcted for comparison.
  * Return: pointer to the first occuence of the letter c.
  *
  */
@@ -11,12 +12,10 @@ char *_strchr(char *s, char c)
 {
 	int a;
 
-	for (a = 0; s[a] != '\0'; a++)
+	for (a = 0; s[a] != '\0' || s[a] == '\0'; a++)
 	{
 		if (s[a] == c)
 			return (&s[a]);
 	}
-		if (s[a] == c)
-			return (&s[a]);
 		return (NULL);
 }
