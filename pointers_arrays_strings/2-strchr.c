16ed7dcda1e69502d9815058a3880e3329121133
Matthew Ernst <6628@holbertonstudents.com>
2023-06-08 09:59:27 -0700

    retun_c_or_null

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strchr - searches string till it find a char def by c or null if not.
+ * @s: sting of info
+ * Return: pointer to the first occuence of the letter c.
+ *
+ */
+
+char *_strchr(char *s, char c)
+
+{
+	int a;
+
+	for (a = 0; s[a] != c; a++)
+	{
+		if s[a] = c;
+		return (a)
+	}
+	return (NULL);
+}
