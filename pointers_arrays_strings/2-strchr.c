dcaeeecbc7cd3f43cdc2e7f2a9ed269275d85c96
Matthew Ernst <6628@holbertonstudents.com>
2023-06-08 11:27:59 -0700

    pointer_to_pointer

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -7,10 +7,13 @@
  *
  */
 
-char* _strchr(char* s, char c) {
+char* _strchr(char* s, char c) 
+{
     int a;
-    for (a = 0; s[a] != '\0'; a++) {
-        if (s[a] == c) {
+    for (a = 0; s[a] != '\0'; a++)
+    {
+	    if (s[a] == c)
+	{
             return &s[a];
         }
     }
