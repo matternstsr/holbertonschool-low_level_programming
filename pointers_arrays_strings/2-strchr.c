f2408006b3f9c36b4ef600a8a70e0fe7ffb97eb5
Matthew Ernst <6628@holbertonstudents.com>
2023-06-08 12:05:16 -0700

    the_astric_for_the_pointer_in_the_proto_was_typoed

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -7,17 +7,15 @@
  *
  */
 
-char* _strchr(char* s, char c) 
+char* _strchr(char *s, char c) 
 {
-    int a;
-    for (a = 0; s[a] != '\0'; a++)
-    {
-	    if (s[a] == c)
+	int a;
+	for (a = 0; s[a] != '\0'; a++)
 	{
-            return &s[a];
-        }
-	    if (s[a] == c)
-		    return ('\0');
-    }
+		if (s[a] == c)
+			return &s[a];
+		if (s[a] == c)
+			return ('\0');
+	}
     return NULL;
 }
