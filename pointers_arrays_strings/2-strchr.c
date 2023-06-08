cd8ff5dfcd7334eabdeba5edb03cb3494d3a40eb
Matthew Ernst <6628@holbertonstudents.com>
2023-06-08 12:16:09 -0700

    added_()_and_fixeda_typo_in_the_proto_again

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -7,15 +7,16 @@
  *
  */
 
-char* _strchr(char *s, char c) 
+char *_strchr(char *s, char c) 
 {
 	int a;
+
 	for (a = 0; s[a] != '\0'; a++)
 	{
 		if (s[a] == c)
-			return &s[a];
+			return (&s[a]);
 	}
 		if (s[a] == c)
-			return &s[a];
-		return NULL;
+			return (&s[a]);
+		return (NULL);
 }
