0579a53b2882a1e2ad32cf61281983d2783699b3
Matthew Ernst <6628@holbertonstudents.com>
2023-06-08 09:04:18 -0700

    point_to_pointer

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _memset - fills desiganted bytes with desiganted values.
+ * @s: pointer to the memeory location
+ * @n: number of bytes to fill of memory.
+ * @b: what the bytes are filled with designated by n.
+ * Return: pointer to the memeory address afftect n.
+ *
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+
+{
+	int a;
+
+	for (a = 0; a < n; a++)
+	{
+		s[a] = b;
+	}
+	return ()
+
+
+}
