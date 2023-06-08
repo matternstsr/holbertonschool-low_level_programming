9298e1db8c8c3d59765b21f000fec8e2b04e958f
Matthew Ernst <6628@holbertonstudents.com>
2023-06-08 16:09:58 -0700

    thik_this_is_going_to_reset_evertime_it_hits_the_outer_loop_but_ill_try_it

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -12,9 +12,19 @@
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int i = 0;
+	unsigned int j = 0;
 	while (s[i] != '\0')
 	{
-		if (strchr(accept, s[i]) == NULL)
+		j = 0;
+		while (accept[j] != '\0')
+		{
+			if (s[i] == accept[j])
+			{
+				break;
+			}
+			j++;
+		}
+		if (accept[j] == '\0')
 		{
 			return i;
 		}
