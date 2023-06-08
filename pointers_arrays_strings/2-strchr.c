f2462b1d247c5689ef9c4880178c47f9369a6694
Matthew Ernst <6628@holbertonstudents.com>
2023-06-08 11:58:26 -0700

    added_the_0_byte_with_an_if_after_the_recalling_if

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -10,12 +10,14 @@
 char* _strchr(char* s, char c) 
 {
     int a;
-    for (a = 0; s[a] != '\0' || s[a] == '\0'; a++)
+    for (a = 0; s[a] != '\0'; a++)
     {
 	    if (s[a] == c)
 	{
             return &s[a];
         }
+	    if (s[a] == c)
+		    return '\0'
     }
     return NULL;
 }
