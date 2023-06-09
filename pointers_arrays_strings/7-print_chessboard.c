68a3ae78a5894d39f2c4af4bd378efd17b84f816
Matthew Ernst <6628@holbertonstudents.com>
2023-06-09 09:59:38 -0700

    i_hate_chess

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_chessboard - prints the chessboard
+ * @a: chessboard string
+ * Return: void
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int r;
+	int c;
+	for (r = 0; r < 8; r++)
+	{
+		for (c = 0; c < 8; c++)
+		{
+			_putchar(a[c][r]);
+		}
+		_putchar('\n');
+	}
+}
