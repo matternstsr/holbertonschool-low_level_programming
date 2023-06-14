498189495b11ac04e2b8bf648fc56bdb9166337a
Matthew Ernst <6628@holbertonstudents.com>
2023-06-14 06:51:18 -0700

    helps_to_use_the_brackets_and_run_both_commands

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -11,8 +11,9 @@
 void _print_rev_recursion(char *s)
 
 {
-	if (*s != 0)
-	_putchar(*s);
-
-	_print_rev_recursion(s - 1);
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar (s)
+	}
 }
