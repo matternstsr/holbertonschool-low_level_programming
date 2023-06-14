4c02db82e98273127ecd62fa8d060e679ad64c5d
Matthew Ernst <6628@holbertonstudents.com>
2023-06-14 05:51:56 -0700

    print_statement

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_recursion - prints the diagnal sums of a 2d array
+ * @s: print a string with putchar
+ *
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+
+{
+	while (s[i] != '\0')
+		i++
+			_putchar(s)
+}
