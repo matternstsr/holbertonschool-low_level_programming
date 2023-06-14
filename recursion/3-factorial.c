c06fb605bc088a96b62c82582de1b420aff8600a
Matthew Ernst <6628@holbertonstudents.com>
2023-06-14 08:00:30 -0700

    try_that

---
+++ recursion/3-factorial.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * int factorial - prints the srint in reverse using recursion
+ * @s: print a string with putchar using recursion
+ *
+ * Return: void
+ */
+
+
+int factorial(int n)
+	if (n != '0')
+	{
+		return ( *= factorial ( n - 1));
+	}
+	return (0);
+}
