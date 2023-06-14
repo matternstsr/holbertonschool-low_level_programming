fbcc7197ea1f8ecdc8cbf753afcb39caa01301dd
Matthew Ernst <6628@holbertonstudents.com>
2023-06-14 14:42:33 -0700

    try_that_for_a_lib!

--- static_libraries/create_static_lib.sh
+++ static_libraries/create_static_lib.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-#!/bin/bash
-echo "Enter the path of the directory you want to copy files from:"
-read source_dir
-echo "Enter the path of the directory you want to copy files to:"
-read destination_dir
-
-cp -r "$source_dir"/* "$destination_dir"
 gcc -Wall -pedantic -Werror -Wextra -c *.c
 ar -rc libmy.a *.o
