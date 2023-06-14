6085c37bb1f528d32968d23c2213fbd5877a3da5
Matthew Ernst <6628@holbertonstudents.com>
2023-06-14 06:38:51 -0700

    showing_travis_how_irts_done

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -11,12 +11,8 @@
 void _print_rev_recursion(char *s)
 
 {
-	if (*s > 0)
-	{
-		_putchar('\n');
-		return;
-	}
-	_putchar(*s);
-	--s;
-	_print_rev_recursion(s - 1);
+	if (*s != '\0')
+
+	_print_rev_recursion(s + 1);
+	_putchar (*s);
 }
