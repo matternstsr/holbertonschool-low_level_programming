9f8e7f13d6700591b185949b32d2e24e0d1dbf1a
Matthew Ernst <6628@holbertonstudents.com>
2023-06-21 12:18:45 -0700

    do_it_to_it

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * str_concat - Concatenates two strings.
+ * @s1: initial string to be added to.
+ * @s2: string to add to s1.
+
+ * Return: if cant add then NULL. if not then point to strings together.
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *combstr;
+	int i 
+	int ic = 0;
+       	int len = 0;
+
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	for (i = 0; s1[i] || s2[i]; i++)
+		len++;
+
+	combstr = malloc(sizeof(char) * len);
+
+	if (combstr == NULL)
+		return (NULL);
+
+	for (i = 0; s1[i]; i++)
+		combstr[ic++] = s1[i];
+
+	for (i = 0; s2[i]; i++)
+		combstr[ic++] = s2[i];
+
+	return (combstr);
+}
