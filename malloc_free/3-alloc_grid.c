7ed3389f830fd10147d78a565dcb8c2cbc02933f
Matthew Ernst <6628@holbertonstudents.com>
2023-06-21 10:22:28 -0700

    do_it_to_it_since_you_labled_them_right_dummy

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -17,7 +17,7 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	mutli = malloc(sizeof(int *) * height);
+	multi = malloc(sizeof(int *) * height);
 
 	if (multi == NULL)
 		return (NULL);
