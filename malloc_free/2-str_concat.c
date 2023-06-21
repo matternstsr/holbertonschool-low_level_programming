f3df581d31e146b07f8c91be219f32c572c50750
Matthew Ernst <6628@holbertonstudents.com>
2023-06-21 13:55:44 -0700

    last_one_passed_but_the_or_statement_mem_leaked_checker_missed_it

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -20,7 +20,9 @@ char *str_concat(char *s1, char *s2)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	for (i = 0; s1[i] || s2[i]; i++)
+	for (i = 0; s1[i]; i++)
+		len++;
+	for (i = 0; s2[i]; i++)
 		len++;
 	combstr = malloc(sizeof(char) * len);
 	if (combstr == NULL)
