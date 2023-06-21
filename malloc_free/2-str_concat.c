5dfd555da198b0d2b5acbcba2f3dd11b5638938d
Matthew Ernst <6628@holbertonstudents.com>
2023-06-21 12:28:18 -0700

    do_it_to_it

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -13,29 +13,21 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *combstr;
-	int i 
+	int i;
 	int ic = 0;
-       	int len = 0;
-
+	int len = 0;
 	if (s1 == NULL)
 		s1 = "";
-
 	if (s2 == NULL)
 		s2 = "";
-
 	for (i = 0; s1[i] || s2[i]; i++)
 		len++;
-
 	combstr = malloc(sizeof(char) * len);
-
 	if (combstr == NULL)
 		return (NULL);
-
 	for (i = 0; s1[i]; i++)
 		combstr[ic++] = s1[i];
-
 	for (i = 0; s2[i]; i++)
 		combstr[ic++] = s2[i];
-
 	return (combstr);
 }
