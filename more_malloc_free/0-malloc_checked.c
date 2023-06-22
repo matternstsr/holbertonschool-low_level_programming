139edf89fe477dae85c2cf1058c2944311282f41
Matthew Ernst <6628@holbertonstudents.com>
2023-06-22 09:26:04 -0700

    do_it_to_it

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *malloc_checked - unction that allocates memory using malloc.
+ *@b: unsigned int
+ *Return: VOID Pointer
+ */
+
+void *malloc_checked(unsigned int b)
+{
+/* VOID pointer type used for unknown types of bytes */
+/* malloc used to set the amount of memory */
+/* if malloc fails to allocate the needed memory */
+/* exit status value */
+/* returns a pointer if not null */
+
+	void *pointer;
+
+	if (pointer == NULL)
+	exit(98);
+	return (pointer);
+}
