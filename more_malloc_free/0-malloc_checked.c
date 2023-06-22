882f0a137751a4d1ebb2011d277ba3ea902f70cc
Matthew Ernst <6628@holbertonstudents.com>
2023-06-22 10:23:07 -0700

    do_it_to_it_I_DONT_GET_IT

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -17,7 +17,7 @@ void *malloc_checked(unsigned int b)
 /* exit status value */
 /* returns a pointer if not null */
 
-	unsigned int *pointer;
+	void *pointer;
 
 	pointer = malloc(b);
 	if (pointer == NULL)
