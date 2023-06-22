cdfc5a5ddf24d8b720542b72e82568a59ad183e3
Matthew Ernst <6628@holbertonstudents.com>
2023-06-22 11:12:52 -0700

    do_it_to_it

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -12,7 +12,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *string;
 	unsigned int i, sizeofs1 = 0, sizeofs2 = 0;
-	
+
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
@@ -20,20 +20,20 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	while (*(s1 + sizeofs1) != '\0')
 		sizeofs1++;
-	
+
 	while (*(s2 + sizeofs2) != '\0')
 		sizeofs2++;
 
 	if (sizeofs2 >= n)
 		sizeofs2 = n;
+
 	string = malloc(sizeofs1 + n + 1);
-	
+
 	if (string == NULL)
 		return (string);
-	
+
 	for (i = 0; i < sizeofs1; i++)
 		string[i] = s1[i];
-
 	for (i = 0; i < n; i++)
 		string[i + sizeofs1] = s2[i];
 
