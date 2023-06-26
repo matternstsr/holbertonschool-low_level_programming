95c78f8cbb7eff881316970e6e1935b8e06f9d6c
Matthew Ernst <6628@holbertonstudents.com>
2023-06-26 11:35:35 -0700

    do_it_to_it

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -13,7 +13,7 @@ int *array_range(int min, int max)
 {
 	int *arr, inc = 0;
 
-	if (min > max) || (arr == NULL)
+	if ((min > max) || (arr == NULL))
 		return (NULL);
 
 	arr = malloc((sizeof(int) * (max - min)) + sizeof(int));
