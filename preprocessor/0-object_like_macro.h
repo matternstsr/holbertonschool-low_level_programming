0f7c5bd9f0a8afd882c58380eda01eb1ddf87349
Matthew Ernst <6628@holbertonstudents.com>
2023-06-26 12:41:08 -0700

    do_it_to_that_and_try_that

--- preprocessor/0-object_like_macro.h
+++ preprocessor/0-object_like_macro.h
@@ -1,8 +1,7 @@
+/**
+ * Create a header that defines a macro "SIZE" as an abbr for the token 1024
+ */
 #ifndef _HEADER_
 #define _HEADER_
 #define SIZE 1024
 #endif
-
-/**
- * Create a header that defines a macro "SIZE" as an abbr for the token 1024
- */
