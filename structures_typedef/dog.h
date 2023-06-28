46c0ff6ff8308150ccc1dcf7ef1afbf1838d3c93
Matthew Ernst <6628@holbertonstudents.com>
2023-06-28 11:57:14 -0700

    fixed_the_proto_in_main_file_dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -19,7 +19,7 @@ struct dog
 typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-dog_t *dumb_mutt(char *name, float age, char *owner);
+dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 int _strlen(char *s);
 char *_strcpy(char *dest, char *src);
