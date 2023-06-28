715b55bbb8cc90999e99db8f77dc4879b1a6bed9
Matthew Ernst <6628@holbertonstudents.com>
2023-06-28 07:46:13 -0700

    do_it_to_it

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,5 @@
 #ifndef DOG_H
 #define DOG_H
-void init_dog(struct dog *d, char *name, float age, char *owner);
 
 /**
  * struct dog - New type dog
@@ -15,4 +14,5 @@ struct dog
 	float age;
 	char *owner;
 };
+void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif
