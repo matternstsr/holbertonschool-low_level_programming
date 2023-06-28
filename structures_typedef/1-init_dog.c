21ef9a571807577f4008659758d3317ee81818d9
Matthew Ernst <6628@holbertonstudents.com>
2023-06-28 07:41:15 -0700

    do_it_ot_it

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  * array_range - function that allocates memory for an array using malloc
+  * @min: min (included) elements
+  * @max: max (included) elements
+  *
+  * Return: returns the pointer to the newly created array
+  */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+
+
+}
