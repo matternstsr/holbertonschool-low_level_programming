2e2cdecb3e04f2620aa820667fd270ebc70cce3e
Matthew Ernst <6628@holbertonstudents.com>
2023-06-29 09:38:53 -0700

    do_it_to_it

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+
+/**
+  * array_iterator - function that itereates through array while doing action
+  * @array: array of chars running against action
+  * @size: size of the array that we are itterating
+  * @action: what we are doing to the array as we itterate
+  *
+  * Return: 
+  */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int incr = 0;
+
+	if (array != NULL && action != NULL && size > 0)
+	{
+		while (incr < size)
+		{
+			action(array[incr]);
+			incr++;
+		}
+	}
+}
