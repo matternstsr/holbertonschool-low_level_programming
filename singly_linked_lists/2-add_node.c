e7e82daa090be3fc6c53238ae5bdd712076b0776
matternstsr <matternstsr@gmail.com>
2023-07-12 10:53:21 -0700

    trying 2

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+  * list_len - Show the number of elements of a list
+  * @h: A linked list
+  *
+  * Return: The number of elements of a list
+  */
+size_t list_len(const list_t *h)
+{
+	size_t ct = 0;
+
+	while (h)
+	{
+		ct += h = h->next;
+	}
+
+	return (ct);
+}
