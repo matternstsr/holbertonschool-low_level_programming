4bfe3af7005a5e650a52acbd1ac9855ae2221e99
matternstsr <matternstsr@gmail.com>
2023-07-12 09:41:38 -0700

    trying again helps to make the lists.h lol

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -15,12 +15,12 @@ size_t print_list(const list_t *h)
 
 	while (h)
 	{
-		if !(h->string)
+		if !(h->str)
 			printf("[0] (nil)\n");
 		else
-			ct += printf("[%d] %s\n", h->length, h->string);
+			ct += printf("[%d] %s\n", h->len, h->str);
 
-		h = h->nextone;
+		h = h->next;
 	}
 
 	return (ct);
