8e73be3818f2b0220e8efb44fac9cb8e70e5675b
matternstsr <matternstsr@gmail.com>
2023-07-17 15:34:42 -0700

    started_0_over

--- more_singly_linked_lists/0-print_listint.c
+++ more_singly_linked_lists/0-print_listint.c
@@ -1,23 +1,25 @@
-#include <stdlib.h>
 #include <stdio.h>
 #include "lists.h"
 
 /**
- * print_dlistint - Prints all the elements of a double linked list
- * @h: The double linked list to prints
- *
- * Return: The number of nodes of the double linked list
- */
-size_t print_dlistint(const dlistint_t *h)
+  * print_listint - Prints all the elements of a linked list
+  * @h: The head of the linked list
+  *
+  * Return: The number of nodes of the linked list
+  */
+size_t print_listint(const listint_t *h)
 {
-	int length = 0;
+	int count = 0;
 
-	while (h != NULL)
+	if (h != NULL)
 	{
-		printf("%d\n", h->n);
-		++length;
-		h = h->next;
+		while (h)
+		{
+			printf("%d\n", h->n);
+			h = h->next;
+			count++;
+		}
 	}
 
-	return (length);
+	return (count);
 }
