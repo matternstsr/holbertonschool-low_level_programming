3f2e38d8849a9ffa5bb6b71763e0a24b77cf1586
matternstsr <matternstsr@gmail.com>
2023-07-17 15:10:55 -0700

    lists_chahged_format

--- more_singly_linked_lists/lists.h
+++ more_singly_linked_lists/lists.h
@@ -40,10 +40,6 @@ list_t *add_node(list_t **head, const char *str);
 size_t list_len(const list_t *h);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-
-
-
-
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_dnodeint(listint_t **head, const int n);
