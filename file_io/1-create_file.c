33a8935d86facc367bc74de8bed4b76527d5d4cf
matternstsr <matternstsr@gmail.com>
2023-07-17 09:10:59 -0700

    reduced_the_line_to_80

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -9,19 +9,19 @@
   */
 int create_file(const char *filename, char *text_content)
 {
-	int file_description;
+	int file_desc;
 
 	if (!filename)
 		return (-1);
 
-	file_description = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-	if (file_description == -1)
+	file_desc = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (file_desc == -1)
 		return (-1);
 
 	if (text_content)
-		write(file_description, text_content, _strlen(text_content));
+		write(file_desc, text_content, _strlen(text_content));
 
-	close(file_description);
+	close(file_desc);
 	return (1);
 }
 
