e770dae330c982ae324718863a0d98785b9cb67a
matternstsr <matternstsr@gmail.com>
2023-07-17 20:07:05 -0700

    forgot_the_semi_on_5_and_had_the_wrong_var_on_9

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -31,7 +31,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 				{
 					temp_node->next = traverse->next;
 					traverse->next = temp_node;
-					return (new_node);
+					return (temp_node);
 				}
 				traverse = traverse->next;
 				count++;
