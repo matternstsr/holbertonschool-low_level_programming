5c267be7ee8aa1d7e2a52497bab4886785f1f576
matternstsr <matternstsr@gmail.com>
2023-07-17 20:25:22 -0700

    spit_the_comments_due_to_80_char_on_8

--- more_singly_linked_lists/8-sum_listint.c
+++ more_singly_linked_lists/8-sum_listint.c
@@ -2,7 +2,8 @@
 #include "lists.h"
 
 /**
-  * sum_listint - function that returns the sum of all the data (n) of a listint_t linked list.
+  * sum_listint - function that returns the sum of all the
+  * data (n) of a listint_t linked list.
   * @head: end of the list
   *
   * Return: if the list is empty, return 0
