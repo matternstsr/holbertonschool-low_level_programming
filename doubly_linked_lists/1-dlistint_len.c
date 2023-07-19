181081322bfeb83e3f7fcfd77de98ac9d8ef956c
matternstsr <matternstsr@gmail.com>
2023-07-19 10:26:21 -0700

    lenth_like_the_singly

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - returns the number of elements in list
+ * @h: The linked list
+ * Return: The number of elements
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	int count = 0;
+	
+
+	while (h)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
