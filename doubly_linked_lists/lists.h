edb099950e9d8ae3f6d5326a5dbb46c74dddc640
matternstsr <matternstsr@gmail.com>
2023-07-19 20:45:20 -0700

    trying_7_after_temp_node_switch

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -1,3 +1,6 @@
+#ifndef LISTS_H
+#define LISTS_H
+
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,13 +12,13 @@
  * @next: points to the next node
  *
  * Description: doubly linked list node structure
- * 
+ * for Holberton project
  */
 typedef struct dlistint_s
 {
-    int n;
-    struct dlistint_s *prev;
-    struct dlistint_s *next;
+	int n;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
 } dlistint_t;
 
 size_t print_dlistint(const dlistint_t *h);
@@ -28,3 +31,4 @@ int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 
+#endif /* LISTS_H */
