6dbc8090f1bb33915a55f8e0106891cf69ad0100
matternstsr <matternstsr@gmail.com>
2023-07-24 07:30:12 -0700

    trying_3_this_one_ois_hard._failing_compile_local_for_some_reason

---
+++ hash_tables/3-hash_table_set.c
@@ -0,0 +1,49 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - adds to the hash table
+ * @ht: Hash table
+ * @key: The key
+ * @value: value with key
+ * Return: 1 sucess, 0 other
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	unsigned long int ina;
+	hash_node_t *new_table;
+
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+		return (0);
+
+	index = key_index((const unsigned char *) key, ht->size);
+	for (ina = index; ht->array[ina]; ina++)
+	{
+		if (strcmp(ht->array[i]->key, key) == 0)
+		{
+			free(ht->array[ina]->value);
+			ht->array[ina]->value = strdup(value);
+			if (ht->array[ina]->value == NULL)
+				return (0);
+			return (1);
+		}
+	}
+
+	newtable = malloc(sizeof(hash_node_t));
+	if (new_table == NULL)
+		return (0);
+
+	new_table->key = strdup(key);
+	if (newtable->key == NULL)
+		return (0);
+
+	new_table->value = strdup(value);
+	if (newtable->value == NULL)
+		return (0);
+
+	new_table->next = ht->array[index];
+	ht->array[index] = new_table;
+
+	return (1);
+}
