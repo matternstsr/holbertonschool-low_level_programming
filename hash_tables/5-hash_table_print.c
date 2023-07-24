bbef0cc1eeeaa9ed9798e2a84c6ea8624ddbba55
matternstsr <matternstsr@gmail.com>
2023-07-24 08:08:47 -0700

    trying_5_to_see_how_bad_it_is

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,36 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - Prints the selected hash table.
+ * @ht: print directed by the pointer
+ * @key: The key
+ *
+ * Return: The value connected to the key
+ * or NULL if key couldn't be found
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *nodentable;
+	unsigned long int inc;
+	unsigned char insert_comma = 0;
+
+	for (inc = 0; inc < ht->size; inc++)
+	{
+		if (ht->array[inc] != NULL)
+		{
+			if (insert_comma == 1)
+				printf(", ");
+
+			nodentable = ht->array[inc];
+			while (nodentable != NULL)
+			{
+				printf("'%s': '%s'", nodentable->key, nodentable->value);
+				nodntable = nodetable->next;
+				if (nodentable != NULL)
+					printf(", ");
+			}
+			insert_comma = 1;
+		}
+	}
+}
