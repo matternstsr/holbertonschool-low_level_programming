3b3478c2996fa6e8c65c6bc980edc8f993ffa5ae
matternstsr <matternstsr@gmail.com>
2023-07-24 08:40:04 -0700

    trying_without_recur

---
+++ hash_tables/6-hash_table_delete.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - Deletes array,node,table.
+ * @ht: A pointer to the table.
+ */
+
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_table_t *head = ht;
+	hash_node_t *nodetodel, *travistmp;
+	unsigned long int inc;
+	
+	for (inc = 0; inc < ht->size; inc++)
+	{
+		if (ht->array[inc])
+		{
+			nodetodel = ht->array[inc];
+			while (nodetodel != NULL)
+			{
+				travistmp = nodetodel->next;
+				free(nodetodel->key);
+				free(nodetdel->value);
+				free(nodetodel);
+				nodetdel = tmp;
+			}
+		}
+	}
+	free(head->array);
+	free(head);
+}
