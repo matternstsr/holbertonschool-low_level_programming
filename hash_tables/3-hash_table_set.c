1b9dad5b3ab6e1492e4f6436c31cd9eb3378f6a9
matternstsr <matternstsr@gmail.com>
2023-07-24 07:35:28 -0700

    didnt_have__

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -30,7 +30,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		}
 	}
 
-	newtable = malloc(sizeof(hash_node_t));
+	new_table = malloc(sizeof(hash_node_t));
 	if (new_table == NULL)
 		return (0);
 
