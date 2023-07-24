7317db82c060dfa40cf007d4399924fd98297294
matternstsr <matternstsr@gmail.com>
2023-07-24 08:10:10 -0700

    cant_type_-_missed_a_e

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -26,7 +26,7 @@ void hash_table_print(const hash_table_t *ht)
 			while (nodentable != NULL)
 			{
 				printf("'%s': '%s'", nodentable->key, nodentable->value);
-				nodntable = nodetable->next;
+				nodentable = nodetable->next;
 				if (nodentable != NULL)
 					printf(", ");
 			}
