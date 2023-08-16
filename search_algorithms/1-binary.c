c7131c1771e8744205ab6483909e26061d7d4539
matternstsr <matternstsr@gmail.com>
2023-08-16 09:34:27 -0700

    pushin1_no_spaces

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -41,16 +41,16 @@ void print_array(int *array, size_t min, size_t max)
 {
 	size_t idx;
 
-        idx = min;
-        if (idx > max)
-                return;
-        printf("Searching in array: ");
-        while (array && idx <= max)
-        {
-                if (idx > min)
-                        printf(", ");
-                printf("%d", array[idx]);
-                idx++;
-        }
-        printf("\n");
+		idx = min;
+		if (idx > max)
+				return;
+		printf("Searching in array: ");
+		while (array && idx <= max)
+		{
+				if (idx > min)
+						printf(", ");
+				printf("%d", array[idx]);
+				idx++;
+		}
+		printf("\n");
 }
