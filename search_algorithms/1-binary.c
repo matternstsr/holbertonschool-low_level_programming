bf808019631026176292bd1a1668fef1df71a1a5
matternstsr <matternstsr@gmail.com>
2023-08-16 08:32:50 -0700

    pushing_1

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -1,32 +1,27 @@
 #include "search_algos.h"
 
 /**
- * binary_search - search using binary search algorithm
- *
- * @array: array of values
- * @size: size of the array
- * @value: value to search for
+ * binary_search - searching using binary seach
+ * @array: array of values that is provided
+ * @size: size of the array that is provided
+ * @value: value to search for that is provided
  * Return: index of the value, or -1 if not found
  */
 int binary_search(int *array, size_t size, int value)
 {
-    if (!array || size == 0)
-        return -1;
-
-    size_t left = 0;
-    size_t right = size - 1;
-    size_t middle;
-
-    while (left <= right)
-    {
-        middle = left + right / 2;
-
-        if (array[middle] == value)
-            return (middle);
-        else if (array[middle] < value)
-            left = middle + 1;
-        else
-            right = middle - 1;
-    }
-    return (-1);
+	size_t left = 0;
+	size_t right = size - 1;
+	size_t middle;
+	if (!array || size == 0)
+		return -1;
+	while (left <= right)
+		middle = left + right / 2;
+	if (array[middle] == value)
+		return (middle);
+	else if (array[middle] < value)
+		left = middle + 1;
+	else
+		right = middle - 1;
+}
+return (-1);
 }
