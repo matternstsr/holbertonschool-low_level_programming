5e8eaf12dbf1b01c3351d7c82f353307bc9b09f9
matternstsr <matternstsr@gmail.com>
2023-08-16 08:43:04 -0700

    sending_1

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -7,20 +7,23 @@
  * @value: value to search for that is provided
  * Return: index of the value, or -1 if not found
  */
+
 int binary_search(int *array, size_t size, int value)
 {
-	size_t left = 0;
-	size_t right = size - 1;
+	size_t min = 0;
+	size_t max = size - 1;
 	size_t middle;
 	if (!array || size == 0)
 		return -1;
-	while (left <= right)
-		middle = left + right / 2;
+	while (min <= max)
+	{
+		middle = min + max / 2;
 	if (array[middle] == value)
 		return (middle);
 	else if (array[middle] < value)
-		left = middle + 1;
+		min = middle + 1;
 	else
-		right = middle - 1;
+		max = middle - 1;
+	}
 	return (-1);
 }
